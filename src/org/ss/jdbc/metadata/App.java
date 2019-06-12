98f81738c93d3539182e7cbdfa3cb0a749a37015
Jean-Baptiste Tâm MATHIEU <jbtmathieu@gmail.com>
2019-06-12 17:14:11 +0200

    4° commit Metadata

---
+++ src/org/ss/jdbc/metadata/App.java
@@ -0,0 +1,61 @@
+package org.ss.jdbc.metadata;
+
+import com.mysql.cj.x.protobuf.MysqlxPrepare;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class App {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static void main(String... args) throws SQLException {
+
+
+        String url = "jdbc:mysql://localhost:3306/digi-jdbc?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String login = "root";
+        String pwd = "";
+        Connection connection = DriverManager.getConnection(url, login, pwd);
+
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet rs = metaData.getTables(connection.getCatalog(),null,"", null);
+        System.out.println("liste des tables de la base de données : ");
+        while (rs.next()){
+            System.out.print(rs.getString("TABLE_NAME")+ " - ");
+
+        }
+        System.out.println();
+        String response;
+        Statement st = connection.createStatement();
+        do {
+            System.out.println("Entrez le nom de la table :");
+            response = sc.nextLine();
+            //response = "contact";
+            ResultSet resultSet = st.executeQuery("SELECT * FROM "+ response);
+            ResultSetMetaData rsMetadata = resultSet.getMetaData();
+            int count = rsMetadata.getColumnCount();
+            if (count > 0) {
+                System.out.println("Voici les information de la table " + response);
+                for (int i = 1; i <= count; ++i) {
+                    System.out.printf("%-15s", rsMetadata.getColumnName(i) + "[" + rsMetadata.getColumnTypeName(i) + "]"); // au minimum 25 caractères - printf = formaté
+                }
+
+                System.out.println();
+                System.out.println("############################");
+                while (resultSet.next()) {
+                    for (int i = 1; i <= count; ++i) {
+                        System.out.printf("%-20s", resultSet.getString(i));
+                    }
+                    System.out.println();
+                }
+
+            }
+
+
+
+        } while (!response.equalsIgnoreCase("exit"));
+
+
+        connection.close();
+    }
+}
